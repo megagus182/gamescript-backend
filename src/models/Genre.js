4ae962970d957999cc0a55c001ba6bbe4dc5ba45
megagus182 <megagus182@hotmail.com>
2025-07-15 11:57:16 -0600

    se modifica un modelo

--- src/models/Genre.js
+++ src/models/Genre.js
@@ -4,17 +4,20 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('genre', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      unique: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-
-})};
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    unique: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+}, {
+  tableName: 'genres', // fuerza el nombre exacto
+  timestamps: true,    // o false si no tienes createdAt/updatedAt
+});
+};
